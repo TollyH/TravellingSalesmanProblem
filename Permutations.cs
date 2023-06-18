b11df2ca2332eaa30d610f54031ca04890af1303
TollyH <ptolemyhill@sky.com>
2023-06-18 20:27:44 +0100

    Implement permutations method

---
+++ Permutations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TravellingSalesmanProblem
+{
+    public static class Permutations
+    {
+        public static IEnumerable<IEnumerable<T>> IteratePermutations<T>(this IEnumerable<T> sequence)
+        {
+            if (!sequence.Any())
+            {
+                yield return Enumerable.Empty<T>();
+            }
+            else
+            {
+                int index = 0;
+                foreach (T startItem in sequence)
+                {
+                    IEnumerable<T> remainingItems = sequence.Where((_, i) => i != index);
+                    foreach (IEnumerable<T> permutation in remainingItems.IteratePermutations())
+                    {
+                        yield return permutation.Prepend(startItem);
+                    }
+                    index++;
+                }
+            }
+        }
+    }
+}
